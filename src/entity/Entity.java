5052f3495676bb957b1891b3d282e0bbf184f8f5
jesustgr <jesustgreyes@gmail.com>
2024-10-22 11:48:27 -0700

    add collision detection

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,15 +1,16 @@
 package entity;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity { //stores variables used in player, monster, and npc classes
 
 	public int worldX, worldY;
 	public int speed;
-	
 	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2; // to store image files
-	public String direction;
-	
+	public String direction;	
 	public int spriteCounter = 0;
 	public int spriteNum = 1;
+	public Rectangle solidArea;
+	public boolean collisionOn = false;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,7 +1,7 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -25,6 +25,12 @@ public class Player extends Entity{
 		
 		screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
 		screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
+		
+		solidArea = new Rectangle();
+		solidArea.x = 8;
+		solidArea.y = 16;
+		solidArea.width = 32;
+		solidArea.height = 32;
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -61,19 +67,30 @@ public class Player extends Entity{
 			
 			if(keyH.upPressed == true) {
 				direction = "up";
-				worldY -= speed;
 			}
 			else if(keyH.downPressed == true) {
 				direction = "down";
-				worldY += speed;
 			}
 			else if(keyH.leftPressed == true) {
 				direction = "left";
-				worldX -= speed;
 			}
 			else if(keyH.rightPressed == true) {
 				direction = "right";
-				worldX += speed;
+			}
+			
+			// check tile collision
+			collisionOn = false;
+			gp.cChecker.checkTile(this);
+			
+			//if collision is false, player can move
+			if(collisionOn == false) {
+				
+				switch(direction) {
+				case "up": worldY -= speed; break;
+				case "down": worldY += speed; break;
+				case "left": worldX -= speed; break;
+				case "right": worldX += speed; break;
+				}
 			}
 			
 			// alternate spriteNum (1 & 2) every 12 frames to create walking animation
